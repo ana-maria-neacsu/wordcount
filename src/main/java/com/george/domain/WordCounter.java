c332336a71869c420e2f0f38c7155ff75d4216dc
juliovidal-n26 <juliocvidal@gmail.com>
2020-12-15 06:09:35 -0300

    [iteration-1] - refactor the count method to use stream

--- src/main/java/com/george/domain/WordCounter.java
+++ src/main/java/com/george/domain/WordCounter.java
@@ -1,21 +1,13 @@
 package com.george.domain;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class WordCounter {
 
     public long count(String word) {
-        String[] words = word.split(" ");
-        long totalWords = 0;
-        for (String s : words){
-            if (s.matches("[a-zA-Z]+")){
-                totalWords++;
-            }
-        }
-
-//        long totalWords = Arrays.stream(words).filter(s -> s.matches("[a-zA-Z]")).count();
-        return totalWords;
-
+        return Arrays.stream(
+                word.split(" "))
+                .filter(s -> s.matches("[a-zA-Z]+"))
+                .count();
     }
 }
