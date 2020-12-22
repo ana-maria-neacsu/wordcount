a6425f50d901816de19caa13d33644280eebc3d4
Jakub Morvay <jakub.morvay@gmail.com>
2020-12-22 11:02:41 +0100

    Add ability to filter out stop words

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ https://ccd-school.de/coding-dojo/agility-katas/word-count-i/
 Assumptions:
 * We define dot, comma and space as separators.
 * All whitespace characters are separators.
+* Stop words and actual words are case-insensitive.
+* We do not do any processing of stopwords.txt file. Incorrectly typed entries (e.g. with trailing whitespace) will be
+not corrected.
--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Foo {
 
@@ -6,15 +11,38 @@ public class Foo {
         System.out.print("Enter text: ");
         final Scanner scanner = new Scanner(System.in);
         final String input = scanner.nextLine();
-        System.out.println("Number of words: " + countWords(input));
+        System.out.println("Number of words: " + countWords(input, getStopWords()));
     }
 
-    static int countWords(final String line) {
+    static Set<String> getStopWords() {
+        final Scanner scanner;
+        try {
+            scanner = new Scanner(new File("stopwords.txt"));
+        } catch (final FileNotFoundException ex) {
+            System.out.println("stopwords.txt file not found, defaulting to empty stop words.");
+            return new HashSet<>();
+        }
+
+        final Set<String> stopWords = new HashSet<>();
+        while (scanner.hasNextLine()) {
+            stopWords.add(scanner.nextLine().toLowerCase());
+        }
+
+        return stopWords;
+    }
+
+    static int countWords(final String line, final Set<String> stopWords) {
         final String[] possibleWords = line.split(",|\\.|\\s+");
+
+        // just in case convert all stop words into the lowercase - the match should be case insensitive
+        final Set<String> loverCaseStopWords = stopWords.stream().map(String::toLowerCase).collect(Collectors.toSet());
 
         int countOfActualWords = 0;
         for (final String possibleWord: possibleWords) {
-            if (possibleWord.matches("([a-z]|[A-Z])+")) {
+            // convert to lowercase, so we can more easily check if the word is actual word and if it is not in stop
+            // words, these should be already lowercase
+            final String lowerCaseWord = possibleWord.toLowerCase();
+            if (lowerCaseWord.matches("[a-z]+") && !loverCaseStopWords.contains(lowerCaseWord)) {
                 countOfActualWords++;
             }
         }
