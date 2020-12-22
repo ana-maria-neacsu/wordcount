cdf33e8e00d473b82288394a16aed901e240acf1
Jakub Morvay <jakub.morvay@gmail.com>
2020-12-22 10:02:06 +0100

    WIP: first prototype of wordCount method

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Foo {
+
+    public static void main(final String[] args) {
+        System.out.print("Enter text: ");
+        final Scanner scanner = new Scanner(System.in);
+        final String input = scanner.nextLine();
+        System.out.println("Number of words: " + wordCount(input));
+    }
+
+    static int wordCount(final String line) {
+        final String[] possibleWords = line.split(",|\\.|\\s+");
+
+        int count = 0;
+        for (final String possibleWord: possibleWords) {
+            if (possibleWord.matches("[a-z]+|[A-Z]+")) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,2 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FooTest {
+
+    @Test
+    public void countWordTest() {
+        // ordinary lines
+        Assert.assertEquals(4, Foo.wordCount("Mary had a lamb"));
+        Assert.assertEquals(4, Foo.wordCount("Mary, had a lamb."));
+        Assert.assertEquals(1, Foo.wordCount("Mary"));
+
+        // weird input
+        Assert.assertEquals(4, Foo.wordCount(".Mary.had,a lamb"));
+        Assert.assertEquals(0, Foo.wordCount(""));
+        Assert.assertEquals(0, Foo.wordCount(".,"));
+        Assert.assertEquals(4, Foo.wordCount(".Mary\thad,a lamb"));
+        Assert.assertEquals(4, Foo.wordCount("\tMary\t\t     had.    "));
+        Assert.assertEquals(3, Foo.wordCount("Mary h$d & lamb"));
+    }
 }
