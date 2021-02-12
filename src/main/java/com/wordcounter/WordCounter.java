385fde8f09cbb70248bcdeb85a2baecd3fe14e9b
Natalija Rozok <natalija.rozok@applifting.czk>
2021-02-12 11:53:38 +0100

    WIP: Add check of word syntaxis to WordCounter class

--- src/main/java/com/wordcounter/WordCounter.java
+++ src/main/java/com/wordcounter/WordCounter.java
@@ -3,11 +3,15 @@ package com.wordcounter;
 import com.wordcounter.TextParser.TextParser;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WordCounter {
 
     private String text;
     private TextParser parser;
+    private List<String> words;
 
     public WordCounter(String text, TextParser parser) {
         this.text = text;
@@ -16,10 +20,24 @@ public class WordCounter {
 
     public Long countWords() {
         List<String> tokens = this.getTokens();
-        return Long.valueOf(tokens.size());
+        this.words = this.convertTokensToWords(tokens);
+        return Long.valueOf(this.words.size());
     }
 
     private List<String> getTokens() {
-        return parser.getTokensWithCollection(text);
+        return this.parser.getTokensWithCollection(text);
+    }
+
+    private List<String> convertTokensToWords(List<String> tokens) {
+        return tokens.stream()
+                .filter(token -> this.checkTokenIsWord(token) == true)
+                .collect(Collectors.toList());
+    }
+
+    private Boolean checkTokenIsWord(String token) {
+        String pattern = "[a-zA-Z]+";
+        Pattern wordPattern = Pattern.compile(pattern);
+        Matcher wordPatternMatcher = wordPattern.matcher(token);
+        return wordPatternMatcher.matches();
     }
 }
