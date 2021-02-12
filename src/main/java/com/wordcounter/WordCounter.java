9a7b1de0c8556d3c0c62abae3288e5fac21a67e1
Natalija Rozok <natalija.rozok@applifting.czk>
2021-02-12 10:07:38 +0100

    WIP: Add first failing test

---
+++ src/main/java/com/wordcounter/WordCounter.java
@@ -0,0 +1,4 @@
+package com.wordcounter;
+
+public class WordCounter {
+}
---
+++ src/test/java/com/wordcounter/test/WordcountTest.java
@@ -0,0 +1,11 @@
+package com.wordcounter.test;
+
+import org.junit.jupiter.api.Test;
+
+public class WordCountTest {
+
+    @Test
+    public void when_2_words_input_than_count_2() {
+    }
+
+}
