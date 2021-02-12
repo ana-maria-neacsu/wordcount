0d59d2a6b469928ef74f84190cbcbac311c5c45d
Natalija Rozok <natalija.rozok@applifting.czk>
2021-02-12 10:20:45 +0100

    WIP: added first failing test

--- src/main/java/com/wordcounter/WordCounter.java
+++ src/main/java/com/wordcounter/WordCounter.java
@@ -1,4 +1,8 @@
 package com.wordcounter;
 
 public class WordCounter {
+
+    public Long count(String text) {
+        return 0L;
+    }
 }
---
+++ src/test/java/com/wordcounter/test/WordCountTest.java
@@ -0,0 +1,20 @@
+package com.wordcounter.test;
+
+import com.wordcounter.WordCounter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordCountTest {
+
+    @Test
+    public void when_2_words_input_than_count_2() {
+        String textInput = "word word";
+
+        WordCounter counter = new WordCounter();
+        Long wordsNumber = counter.count(textInput);
+
+        assertEquals(wordsNumber, 2);
+    }
+
+}
