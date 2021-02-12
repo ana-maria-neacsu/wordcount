960097beeaef1eb9dfa07e1da5b3cb9f614e08d3
Natalija Rozok <natalija.rozok@applifting.czk>
2021-02-12 11:03:06 +0100

    WIP: Refactor of WordCounter class

--- src/main/java/com/wordcounter/WordCounter.java
+++ src/main/java/com/wordcounter/WordCounter.java
@@ -7,10 +7,21 @@ import java.util.List;
 
 public class WordCounter {
 
+    private TextParser parser;
+
+    public WordCounter(TextParser parser) {
+        this.parser = parser;
+    }
+
     public Long count(String text) {
 
-        TextParser parser = new TextParserImpl(text, " ");
-        List<String> tokens = parser.getTokensWithCollection();
+//        TextParser parser = new TextParserImpl(text, " ");
+        List<String> tokens = this.getTokens();
+
         return Long.valueOf(tokens.size());
+    }
+
+    private List<String> getTokens() {
+        return parser.getTokensWithCollection();
     }
 }
--- src/main/java/com/wordcounter/WordCounterDemo.java
+++ src/main/java/com/wordcounter/WordCounterDemo.java
@@ -1,8 +1,12 @@
 package com.wordcounter;
 
+import com.wordcounter.TextParser.TextParser;
+import com.wordcounter.TextParser.TextParserImpl;
+
 public class WordCounterDemo {
 
     public static void main(String[] args) {
+        TextParser parser = new TextParserImpl("text", " ");
 
     }
 }
