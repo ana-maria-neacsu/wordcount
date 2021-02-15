0288d1764ae19bcaf891420288784b926ba71f87
jozefCsupka <jozefcsupka@gmail.com>
2021-02-15 09:48:57 +0100

    Test method int FooTest

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FooTest {
+    @Test
+    public void testFooMethod(){
+        int testInt = 1;
+        Assertions.assertEquals(1, testInt);
+    }
 }
