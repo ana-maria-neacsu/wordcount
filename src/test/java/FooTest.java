ceef082bc0cd32adc2ef5da42d087adb1ebbb702
hessam-shm <hessam@ymail.com>
2021-03-10 12:27:44 +0330

    intitial setup

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FooTest {
+    @Test
+    public void sampleTest(){
+        Assertions.assertTrue(true);
+    }
 }
