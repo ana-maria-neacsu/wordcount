9360efe397b7d1be293973b1c69840097a32e016
hessam-shm <hessam@ymail.com>
2021-03-10 14:02:17 +0330

    word count complete tests

--- src/test/java/service/WordCountServiceTest.java
+++ src/test/java/service/WordCountServiceTest.java
@@ -1,16 +1,42 @@
 package service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class WordCountServiceTest {
 
     WordCountService wordCountService = new WordCountService();
-    @ParameterizedTest
-    @CsvSource({"word count,2", "123 count,1", "foo,1"})
-    public void countWordsTest(String text, String expected){
-        Assertions.assertEquals(Integer.parseInt(expected),wordCountService.countWords(text));
+
+    @Test
+    public void countWordEmptyTextTest() {
+        Assertions.assertEquals(0, wordCountService.countWords(""));
+    }
+
+    @Test
+    public void countWordWithNumbersTest() {
+        Assertions.assertEquals(1, wordCountService.countWords("word 123"));
+    }
+
+    @Test
+    public void countWordTest() {
+        Assertions.assertEquals(2, wordCountService.countWords("word count"));
+    }
+
+    @Test
+    public void countWordWithSpecialCharacterTest() {
+        Assertions.assertEquals(0, wordCountService.countWords("word, \""));
+    }
+
+    @Test
+    public void countWordWithNonEnglishAlphabet(){
+        Assertions.assertEquals(1, wordCountService.countWords("test خسام"));
+    }
+
+    @Test
+    public void countWordRequirement(){
+        Assertions.assertEquals(5, wordCountService.countWords("Mary had a little lamb"));
     }
 
 }
