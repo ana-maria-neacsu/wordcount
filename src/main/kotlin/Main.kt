8a0482d9a9ff951e4e7525ff39e2e8fbcde0b2ea
CLANCE\dklempai <darius.klempai@cleverlance.com>
2021-03-15 13:39:38 +0100

    implemented stop words in configuration

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,33 @@
-import domain.WordsCountUseCase
-import domain.impl.AllTokensCountUseCase
-import domain.impl.ConfigurableWordsCountUseCase
-import domain.impl.TextByWhitespaceSplitUseCase
-import domain.impl.ValidWordTokensFilterUseCase
+import domain.WordsCountGetUseCase
+import domain.impl.*
 import input.TextInput
 import input.commandline.CommandLineTextInput
+import input.file.FileTextInput
 import output.TextOutput
 import output.commandline.CommandLineTextOutput
 
 fun main() {
     val input: TextInput = CommandLineTextInput()
     val output: TextOutput = CommandLineTextOutput()
-    val wordsCountUseCase: WordsCountUseCase = ConfigurableWordsCountUseCase(
+
+    val wordsCountGetUseCase: WordsCountGetUseCase = ConfigurableWordsCountGetUseCase(
         textSplitUseCase = TextByWhitespaceSplitUseCase(),
-        tokensFilterUseCase = ValidWordTokensFilterUseCase(),
+        tokensFilterUseCase = CompositeTokensFilterUseCase(
+            listOf(
+                ValidWordTokensFilterUseCase(),
+                NonStopWordTokensFilterUseCase(
+                    stopWordsGetUseCase = TextInputStopWordsGetUseCase(
+                        textInput = FileTextInput("stopwords.txt"),
+                        textSplitUseCase = TextByNewLineSplitUseCase()
+                    )
+                )
+            )
+        ),
         tokensCountUseCase = AllTokensCountUseCase()
     )
 
     output.showText("Enter text: ")
 
-    val result = wordsCountUseCase.countWords(input.getText())
+    val result = wordsCountGetUseCase.getWordCount(input.getText())
     output.showText("Number of words: $result\n")
 }
