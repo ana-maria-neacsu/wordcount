c6130b0be439e7c26e40c26df9749bb0f6139b2b
Aleksandar Putnik <aleksandar.putnik@dedalus-group.com>
2021-03-29 12:10:08 +0200

    Initial implementation of WordCounterApp

--- src/main/java/WordCounterApp.java
+++ src/main/java/WordCounterApp.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+import wordcounter.WordCounter;
+
 public class WordCounterApp {
 
     public static void main(String[] args) {
         // 1. ask user for input
+        System.out.print("Enter text: ");
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
 
         // 2. process the input
+        WordCounter wordCounter = new WordCounter();
+        int count = wordCounter.count(text);
 
         // 3. print out the result
+        System.out.println("Number of words: " + count);
     }
 }
