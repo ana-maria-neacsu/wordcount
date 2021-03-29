3ad1e63b81b2e00300f40478db9548e1fee8db66
Aleksandar Putnik <aleksandar.putnik@dedalus-group.com>
2021-03-29 15:01:32 +0200

    Extend WordCounter to use stop words

--- src/main/java/wordcounter/WordCounter.java
+++ src/main/java/wordcounter/WordCounter.java
@@ -1,6 +1,8 @@
 package wordcounter;
 
+import java.util.Collections;
 import java.util.Objects;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import static java.util.Arrays.stream;
@@ -11,16 +13,28 @@ public class WordCounter {
     public static final String EMPTY_SPACES = "\\s+";
 
     private final String text;
+    private final Set<String> stopWords;
 
     /**
-     *
-     * This constructor returns accepts input text and the stopWords.
-     *
+     * Default value for stop words is taken - empty set
      * @param text input text
      */
     public WordCounter(String text) {
-        Objects.requireNonNull(text, "text must not be null");
+        this(text, Collections.emptySet());
+    }
+
+    /**
+     *
+     * This constructor accepts input text and the stop words (words to be skipped).
+     *
+     * @param text input text
+     * @param stopWords list of words that are not counted
+     */
+    public WordCounter(String text, Set<String> stopWords) {
+        Objects.requireNonNull(text, "text must not be null!");
+        Objects.requireNonNull(stopWords, "stopWords must not be null!");
         this.text = text;
+        this.stopWords = stopWords;
     }
 
     /**
@@ -43,6 +57,9 @@ public class WordCounter {
     }
 
     private boolean isCorrectWord(String word) {
+        if (stopWords.contains(word)) {
+            return false;
+        }
         return WORD_PATTERN.matcher(word).matches();
     }
 }
--- src/test/java/wordcounter/WordCounterTest.java
+++ src/test/java/wordcounter/WordCounterTest.java
@@ -1,15 +1,24 @@
 package wordcounter;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WordCounterTest {
 
     @Test
-    public void should_throw_exception_when_null_is_provided() {
+    public void should_throw_exception_when_null_is_provided_for_text() {
         assertThrows(NullPointerException.class, () -> createWordCounterWith(null));
+    }
+
+    @Test
+    public void should_throw_exception_when_null_is_provided_for_stop_words() {
+        assertThrows(NullPointerException.class, () -> createWordCounterWith("some text", null));
     }
 
     @Test
@@ -53,8 +62,24 @@ public class WordCounterTest {
         assertEquals(1, createWordCounterWith("Hi he11o, Dav!d").count());
     }
 
+    @Test
+    public void should_return_0_when_only_stop_words_are_given() {
+        Set<String> stopWords = new HashSet<>(asList("Hi", "we", "are", "stop", "words"));
+        assertEquals(0, createWordCounterWith("Hi we are stop words", stopWords).count());
+    }
+
+    @Test
+    public void should_return_1_for_one_regular_word_and_one_stop_word() {
+        Set<String> stopWords = new HashSet<>(asList("Hi"));
+        assertEquals(1, createWordCounterWith("Hi there", stopWords).count());
+    }
+
     private WordCounter createWordCounterWith(String text) {
-        return new WordCounter(text);
+        return createWordCounterWith(text, Collections.emptySet());
+    }
+
+    private WordCounter createWordCounterWith(String text, Set<String> stopWords) {
+        return new WordCounter(text, stopWords);
     }
 
 }
