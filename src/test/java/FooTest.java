fc299042a4b0e9761b6e2347290a8fe1294e3811
Sorin VEGHIU <sveghiu@mantu.com>
2021-04-02 09:47:40 +0200

    Added Test method

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FooTest {
+	
+	@Test
+	public void TestSomething() {
+	
+		assertTrue(true);
+	}
+	
 }
